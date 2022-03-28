fb3982bb763d7ef2eb0695ab84772cc17711f5eb
hugosenari <hugosenari@gmail.com>
2022-03-27 21:55:10 -0300

    chore: re index playground

--- master/flake.nix
+++ master/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-playground-master.ref   = "refs/heads/master";
   inputs.src-playground-master.owner = "theduke";
   inputs.src-playground-master.repo  = "nim-playground";
-  inputs.src-playground-master.dir   = "";
   inputs.src-playground-master.type  = "github";
   
   inputs."jester".owner = "nim-nix-pkgs";
--- v0_1_0/flake.nix
+++ v0_1_0/flake.nix
@@ -11,7 +11,6 @@
   inputs.src-playground-v0_1_0.ref   = "refs/tags/v0.1.0";
   inputs.src-playground-v0_1_0.owner = "theduke";
   inputs.src-playground-v0_1_0.repo  = "nim-playground";
-  inputs.src-playground-v0_1_0.dir   = "";
   inputs.src-playground-v0_1_0.type  = "github";
   
   inputs."jester".owner = "nim-nix-pkgs";
